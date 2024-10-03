77a2ffbacb8133d906fc753abb5be1fabb105112
Stefan Galler <galler.stefan@gmail.com>
2024-10-03 07:58:04 +0200

    remove implementation from auth_interceptor and auth_controller

--- lib/auth/interceptor/auth_interceptor.dart
+++ lib/auth/interceptor/auth_interceptor.dart
@@ -1,7 +1,4 @@
-import 'dart:developer';
-
 import 'package:dio/dio.dart';
-import 'package:flutter_jwt_auth/auth/state/auth_controller.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class AuthInterceptor extends Interceptor {
@@ -13,42 +10,13 @@ class AuthInterceptor extends Interceptor {
   @override
   void onRequest(
       RequestOptions options, RequestInterceptorHandler handler) async {
-    final authData = await ref.read(authControllerProvider.future);
-    final token = authData?.token;
-    if (token != null) {
-      options.headers['Authorization'] = 'Bearer $token';
-    }
-
+    // TODO: add token as Authorized header
     super.onRequest(options, handler);
   }
 
   @override
   void onError(DioException err, ErrorInterceptorHandler handler) async {
-    final isRetry = err.requestOptions.extra['isRetry'] == true;
-
-    // if response is unauthorized
-    if (err.response?.statusCode == 401 && !isRetry) {
-      try {
-        final authData = await ref.read(authControllerProvider.future);
-        final refreshToken = authData?.refreshToken;
-
-        if (refreshToken != null) {
-          await ref.read(authControllerProvider.notifier).refreshToken();
-
-          final options = err.requestOptions;
-          options.extra['isRetry'] = true;
-          final response = await dio.fetch(options);
-
-          handler.resolve(response);
-        } else {
-          super.onError(err, handler);
-        }
-      } on DioException catch (error, trace) {
-        log('cannot refresh', error: error, stackTrace: trace);
-        super.onError(error, handler);
-      }
-    } else {
-      super.onError(err, handler);
-    }
+    // TODO: implement refresh token handling, if token is invalid
+    super.onError(err, handler);
   }
 }
--- lib/auth/state/auth_controller.dart
+++ lib/auth/state/auth_controller.dart
@@ -1,7 +1,4 @@
-import 'package:dio/dio.dart';
 import 'package:flutter_jwt_auth/auth/model/auth_data.dart';
-import 'package:flutter_jwt_auth/auth/repository/auth_repository.dart';
-import 'package:flutter_jwt_auth/common/repository/storage_repository.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'auth_controller.g.dart';
@@ -10,54 +7,19 @@ part 'auth_controller.g.dart';
 class AuthController extends _$AuthController {
   @override
   Future<AuthData?> build() async {
-    final storageRepository = ref.watch(storageRepositoryProvider);
-
-    final token = await storageRepository.getToken();
-    final refreshToken = await storageRepository.getRefreshToken();
-
-    if (token != null && refreshToken != null) {
-      return AuthData(token: token, refreshToken: refreshToken);
-    } else {
-      return null;
-    }
+    // TODO: load tokens from storage
+    return null;
   }
 
   Future<void> login(String username) async {
-    final authRepository = ref.read(authRepositoryProvider);
-    final storageRepository = ref.read(storageRepositoryProvider);
-
-    state = await AsyncValue.guard(() async {
-      final authData = await authRepository.login(username);
-      await storageRepository.storeToken(authData.token);
-      await storageRepository.storeRefreshToken(authData.refreshToken);
-      return authData;
-    });
+    // TODO: implement login
   }
 
   Future<void> refreshToken() async {
-    final authRepository = ref.read(authRepositoryProvider);
-    final storageRepository = ref.read(storageRepositoryProvider);
-    final authData = state.valueOrNull;
-    if (authData != null) {
-      try {
-        final newTokens =
-            await authRepository.refreshToken(authData.refreshToken);
-        storageRepository.storeToken(newTokens.token);
-        storageRepository.storeRefreshToken(newTokens.refreshToken);
-        state = AsyncData(newTokens);
-      } on DioException {
-        await logout();
-        rethrow;
-      }
-    }
+    // TODO: implement refresh token
   }
 
   Future<void> logout() async {
-    state = await AsyncValue.guard(() async {
-      final storageRepository = ref.read(storageRepositoryProvider);
-      await storageRepository.deleteToken();
-      await storageRepository.deleteRefreshToken();
-      return null;
-    });
+    // TODO: implement logout
   }
 }
